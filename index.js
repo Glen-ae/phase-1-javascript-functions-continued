97e97ad32c298199b9f66be61ff4c4fe3e51da93
Glen <glen.kimani@student.moringaschool.com>
2022-11-26 01:55:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (activity='roller-skate') {
+    return 'This Saturday, I want to ' + activity + '!'
+}
+saturdayFun('Go for a facial');
+
+let mondayWork = function(task = 'go to the office' ){
+    return 'This Monday, I will ' + task + '.'
+}
+console.log(mondayWork ('attend the Microsoft Campus Tour'))
+
+function wrapAdjective (result = '*') {
+    return function (emphatic) {
+        return `You are ${result}${emphatic}${result}!` ;
+    }
+}
+
+wrapAdjective('*')("a hard worker")
+wrapAdjective("||")("a decdicated programmer")
